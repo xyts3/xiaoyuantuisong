bf8b4c5b0203a1f1bc38b6c6a73656d51b4c49e2
kavstall <634774765@qq.com>
2018-01-10 10:25:21 +0800

    test

--- xiaoyuantuisong-common/src/main/java/com/sbs/xyts/mapper/Test.java
+++ xiaoyuantuisong-common/src/main/java/com/sbs/xyts/mapper/Test.java
@@ -1,10 +1,10 @@
 package com.sbs.xyts.mapper;
 
 public class Test {
-<<<<<<< HEAD
+
 	public String a;
 	private int aa=11;
-=======
-private String name;
->>>>>>> 43f0fbd585cdedca34cf7161ab73d8f73e38aa28
+
+	private String name;
+
 }
